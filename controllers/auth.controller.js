23e905c25a246c8138e61f4525a5f8fefb48208a
Killian Galea <kgalea@steamulo.com>
2020-05-28 14:53:15 +0200

    [FEAT] Use bcrypt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,14 @@
 const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
 
 exports.postSignup = async(req, res, next) => {
     if (!req.body || !req.body.username || !req.body.email || !req.body.password) return res.sendStatus(400);
 
+    let salt = bcrypt.genSaltSync(12);
+    let hash = bcrypt.hashSync(req.body.password, salt);
+
     const user = new User({... req.body});
+    user.password = hash;
 
     user.save().then(data => {
         res.status(201).json({
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-const { DB_HOST, DB_NAME, DB_URL, DB_USER, DB_PASS } = process.env;
+const { DB_HOST, DB_NAME, DB_URL, DB_USER, DB_PASS, SECRET, JWT_SECRET } = process.env;
 
 const db = `${DB_HOST}://${DB_USER}:${DB_PASS}@${DB_URL}/${DB_NAME}?retryWrites=true&w=majority`;
 
-module.exports = { db };
+module.exports = { db, SECRET, JWT_SECRET };
